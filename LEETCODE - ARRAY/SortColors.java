42509f636a4b77ff3f7c4e8ff04a4a4d823fd1f5
aravind452 <aravindaru452@gmail.com>
2024-08-06 10:55:25 +0530

    added sort colors

---
+++ LEETCODE - ARRAY/SortColors.java
@@ -0,0 +1,34 @@
+public class SortColors {
+  public static void main(String[] args) {
+    int[] nums = { 2,0,1};
+    int zeros = 0, ones = 0, twos = 0;
+
+    for(int i=0;i<nums.length;i++){
+      if(nums[i] == 0){
+        zeros++;
+      }
+      if(nums[i] == 1){
+        ones++;
+      }
+      if(nums[i] == 2){
+        twos++;
+      }
+    }
+
+    for(int i=0;i<zeros;i++){
+      nums[i] = 0;
+    }
+    for(int i=zeros;i<zeros+ones;i++){
+      nums[i] = 1;
+    }
+    for(int i=zeros+ones;i<nums.length;i++){
+      nums[i] = 2;
+    }
+
+
+    for(int i=0;i<nums.length;i++){
+      System.out.println(nums[i]);
+    }
+  }
+
+}
