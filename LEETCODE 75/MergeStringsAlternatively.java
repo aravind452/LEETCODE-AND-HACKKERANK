7acf272d81e2df9a2d25f5f9cbdad9754b3452ac
aravind452 <aravindaru452@gmail.com>
2024-09-09 22:33:15 +0530

    added merge strings alternatively

---
+++ LEETCODE 75/MergeStringsAlternatively.java
@@ -0,0 +1,24 @@
+public class MergeStringsAlternatively {
+
+  public static void main(String[] args) {
+    String s1 = "abc";
+    String s2 = "pqrs";
+
+    int length1 = s1.length();
+    int length2 = s2.length();
+
+    int maxLength = Math.max(length1, length2);
+
+    String result = "";
+
+    for (int i = 0; i < maxLength; i++) {
+      if (i < length1) {
+        result += s1.charAt(i);
+      }
+      if (i < length2) {
+        result += s2.charAt(i);
+      }
+    }
+    System.out.println(result);
+  }
+}
