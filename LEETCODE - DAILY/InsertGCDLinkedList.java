c122acb274e39c3880fe852c4f49e025a56082d6
aravind452 <aravindaru452@gmail.com>
2024-09-10 10:58:36 +0530

    added gcd linked list

---
+++ LEETCODE - DAILY/InsertGCDLinkedList.java
@@ -0,0 +1,35 @@
+// /**
+//  * Definition for singly-linked list.
+//  * public class ListNode {
+//  * int val;
+//  * ListNode next;
+//  * ListNode() {}
+//  * ListNode(int val) { this.val = val; }
+//  * ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+//  * }
+//  */
+// class Solution {
+//   public ListNode insertGreatestCommonDivisors(ListNode head) {
+
+//       ListNode temp = head, prev = null;
+//       while (temp != null) {
+//           if (prev != null) {
+//               int val = gcd(prev.val, temp.val);
+//               ListNode newnode = new ListNode(val, temp);
+//               prev.next = newnode;
+//           }
+//           prev = temp;
+//           temp = temp.next;
+//       }
+//       return head;
+
+//   }
+
+//   public int gcd(int a, int b) {
+//       if (b == 0) {
+//           return a;
+//       } else {
+//           return gcd(b, a % b);
+//       }
+//   }
+// }
