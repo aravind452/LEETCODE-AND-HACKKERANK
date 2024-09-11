eb8e6dd1072665c7b1bdbf1e2d1aac783073f5cd
aravind452 <aravindaru452@gmail.com>
2024-09-11 10:39:28 +0530

    added minimum flips

---
+++ LEETCODE - DAILY/MinimumFlips.java
@@ -0,0 +1,27 @@
+class MinimumFlips {
+
+  public static void main(String[] args) {
+
+    int start = 7;
+    int goal = 10;
+    int xor = start ^ goal;
+    // System.out.println(xor);
+    int result = Integer.bitCount(xor);
+    System.out.println(result);
+  }
+
+}
+
+class Solution {
+  public int minBitFlips(int start, int goal) {
+    int xor = start ^ goal;
+    int count = 0;
+    while (xor != 0) {
+      count += xor & 1;
+      xor >>= 1;
+    }
+    return count;
+
+  }
+
+}
