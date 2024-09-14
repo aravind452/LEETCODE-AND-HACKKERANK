31343aff1e96397a2f7858a74c1a8045f1929561
aravind452 <aravindaru452@gmail.com>
2024-09-14 22:12:49 +0530

    modified min flips

--- LEETCODE - DAILY/MinimumFlips.java
+++ LEETCODE - DAILY/MinimumFlips.java
@@ -12,16 +12,16 @@ class MinimumFlips {
 
 }
 
-class Solution {
-  public int minBitFlips(int start, int goal) {
-    int xor = start ^ goal;
-    int count = 0;
-    while (xor != 0) {
-      count += xor & 1;
-      xor >>= 1;
-    }
-    return count;
+// class Solution {
+//   public int minBitFlips(int start, int goal) {
+//     int xor = start ^ goal;
+//     int count = 0;
+//     while (xor != 0) {
+//       count += xor & 1;
+//       xor >>= 1;
+//     }
+//     return count;
 
-  }
+//   }
 
-}
+// }
