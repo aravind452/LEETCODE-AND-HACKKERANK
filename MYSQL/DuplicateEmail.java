abfaaa27f332014b332d2ecb98a5ec936febf86c
aravind452 <aravindaru452@gmail.com>
2024-10-03 15:14:55 +0530

    added duplicate email and customer referee

---
+++ MYSQL/DuplicateEmail.java
@@ -0,0 +1,29 @@
+public class DuplicateEmail {
+
+  public static void main(String[] args) {
+    /*
+     * Input:
+     * Person table:
+     * +----+------------------+
+     * | id | email |
+     * +----+------------------+
+     * | 1 | john@example.com |
+     * | 2 | bob@example.com |
+     * | 3 | john@example.com |
+     * +----+------------------+
+     * Output:
+     * +----+------------------+
+     * | id | email |
+     * +----+------------------+
+     * | 1 | john@example.com |
+     * | 2 | bob@example.com |
+     * +----+------------------+
+     */
+  }
+
+  /*
+   * delete p1 from person p1,person p2
+   * where p1.email=p2.email and p1.id>p2.id;
+   */
+
+}
